3cfc2c753d3655122a02f53cfb96d97a22b966f0
avish-20 <avishsaini2001@gmail.com>
2025-11-30 11:41:48 +0530

    Pushing initial code

---
+++ src/main/java/com/avish/MvcModule2/MvcModule2Application.java
@@ -0,0 +1,13 @@
+package com.avish.MvcModule2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcModule2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcModule2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/avish/MvcModule2/MvcModule2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avish.MvcModule2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcModule2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
